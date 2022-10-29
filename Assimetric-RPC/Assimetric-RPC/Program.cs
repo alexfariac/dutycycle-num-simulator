4863d5db49016699ec42b22327134f6b36bc1cf6
Alexandre Cardoso <alexandre.faria95@gmail.com>
2022-10-29 00:10:47 -0300

    chore: hasEncounter return encounter index

--- Assimetric-RPC/Assimetric-RPC/Program.cs
+++ Assimetric-RPC/Assimetric-RPC/Program.cs
@@ -1,51 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Assimetric_RPC;
+﻿using Assimetric_RPC;
 using System.Text;
 
-//List<int> bd7 = new List<int> { 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0 };
-//List<int> bd21 = new List<int> { 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-var s1 = new Grid(120, 120);
-var s2 = new Disco(109, 113);
+var s1 = new UConnect(107);
+var s2 = new UConnect(107);
 
 List<int> schedule1 = s1.Schedule; 
-List<int> schedule2 = s2.Schedule; 
-
-Console.WriteLine(s1.DutyCyclePerc);
-Console.WriteLine(s2.DutyCyclePerc);
+List<int> schedule2 = s2.Schedule;
 
 for (int i = 0; i < schedule1.Count; i++)
 {
     for (int j = i; j < schedule2.Count; j++)
     {
-        bool b_hasEncounter = hasEncounter(schedule1, schedule2, i, j);
+        int encounterIndex = hasEncounter(schedule1, schedule2, i, j);
 
-        if (!b_hasEncounter)
+        if (encounterIndex == -1)
         {
             Console.WriteLine($"No encounter oportunity at schedule1 offset:{i} and schedule2 offset:{j}.");
-            //print(schedule1, i);
-            //print(schedule2, j);
             Console.WriteLine(String.Empty);
         }
     }
 }
 
 
-static bool hasEncounter(List<int> schedule1, List<int> schedule2, int offset1 = 0, int offset2 = 0)
+static int hasEncounter(List<int> schedule1, List<int> schedule2, int offset1 = 0, int offset2 = 0)
 {
-    int i_lcm = lcm(schedule1.Count, schedule2.Count);
-    for(int i = 0; i < i_lcm; i++)
+    for(int i = 0; i < lcm(schedule1.Count, schedule2.Count); i++)
     {
         int index1 = offsetIndex(schedule1.Count, i + offset1);
         int index2 = offsetIndex(schedule2.Count, i + offset2);
 
         if(schedule1[index1] == 1 && schedule2[index2] == 1)
         {
-            return true;
+            return i;
         }
     }
 
-    return false;
+    return -1;
 }
 
 static int gcf(int a, int b)
