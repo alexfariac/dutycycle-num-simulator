6c3de2719e2bd631c6d3fd1758428217b261b6ca
Alexandre Cardoso <alexandre.faria95@gmail.com>
2022-11-02 17:59:17 -0300

    feat: begin test project grid tests

---
+++ Assimetric-RPC/Assimetric-RPC-Tests/Methods/GridTest.cs
@@ -0,0 +1,50 @@
+﻿using Assimetric_RPC.Methods;
+
+namespace Assimetric_RPC_Tests.Methods
+{
+    public class GridTest
+    {
+        public static List<object[]> GridObjects => new()
+        {
+            new object[]
+            {
+                2,
+                2,
+                new List<bool> 
+                { 
+                    true, true, 
+                    true, false
+                } 
+            },
+            new object[]
+            {
+                5,
+                5,
+                new List<bool> 
+                { 
+                    true, true, true, true, true,
+                    true, false, false, false, false,
+                    true, false, false, false, false,
+                    true, false, false, false, false,
+                    true, false, false, false, false,
+                }
+            }
+        };
+
+        [Theory]
+        [MemberData(nameof(GridObjects))]
+        public void BuildSchedule(int n, int m, List<bool> expectedSchedule)
+        {
+            // Arrange
+
+            // Act
+            Grid grid = new Grid(n, m);
+
+            // Assert
+            Assert.Equal(expectedSchedule.Count, grid.ScheduleSize);
+            Assert.Equal(expectedSchedule, grid.Schedule);
+            Assert.Equal(n * m, grid.ScheduleSize);
+            Assert.Equal(n + m - 1, grid.ActiveSlotsCount);
+        }
+    }
+}
