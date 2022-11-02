3a80bc47be4006af1717d3be4ff3f3b3e15584ce
Alexandre Cardoso <alexandre.faria95@gmail.com>
2022-11-02 16:21:42 -0300

    feat: move math methods to utils folder

---
+++ Assimetric-RPC/Assimetric-RPC/Utils/MathUtils.cs
@@ -0,0 +1,22 @@
+﻿namespace Assimetric_RPC.Utils
+{
+    public static class MathUtils
+    {
+        public static int gcf(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public static int Lcm(int a, int b)
+        {
+            return a / gcf(a, b) * b;
+        }
+
+    }
+}
