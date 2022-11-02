06ae4184cc10ab03b5ac67420fbad53cb666ada5
Alexandre Cardoso <alexandre.faria95@gmail.com>
2022-11-02 17:36:58 -0300

    feat: move rpc methods to class

---
+++ Assimetric-RPC/Assimetric-RPC/HeterogenousRPC.cs
@@ -0,0 +1,43 @@
+﻿using Assimetric_RPC.Methods;
+using Assimetric_RPC.Utils;
+
+namespace Assimetric_RPC
+{
+    public static class HeterogenousRPC
+    {
+        public static void NDTMetrics(ScheduleMethod schedule1, ScheduleMethod schedule2)
+        {
+            int discoveryTimeSum = 0;
+            int maxDiscoveryTime = 0;
+
+            int lcm = MathUtils.Lcm(schedule1.ScheduleSize, schedule2.ScheduleSize);
+
+            for (int relativeOffset = 0; relativeOffset < lcm; relativeOffset++)
+            {
+                int discoveryTime = DiscoveryTime(schedule1, schedule2, relativeOffset);
+                discoveryTimeSum += discoveryTime;
+                maxDiscoveryTime = Math.Max(maxDiscoveryTime, discoveryTime);
+            }
+
+            var averageNdt = discoveryTimeSum / lcm;
+            Console.WriteLine($"DC1: {schedule1.DutyCyclePerc}, DC2: {schedule2.DutyCyclePerc}, AVG-NDT: {averageNdt}, MAX-NDT: {maxDiscoveryTime}");
+        }
+
+        public static int DiscoveryTime(ScheduleMethod schedule1, ScheduleMethod schedule2, int relativeOffset = 0)
+        {
+            for(int i = 0; i < MathUtils.Lcm(schedule1.ScheduleSize, schedule2.ScheduleSize); i++)
+            {
+                int index1 = i % schedule1.ScheduleSize;
+                int index2 = (i + relativeOffset) % schedule2.ScheduleSize;
+
+                if(schedule1.Schedule[index1] && schedule2.Schedule[index2])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
--- Assimetric-RPC/Assimetric-RPC/Program.cs
+++ Assimetric-RPC/Assimetric-RPC/Program.cs
@@ -1,63 +1,12 @@
 ﻿using Assimetric_RPC;
+using Assimetric_RPC.Methods;
 using System.Text;
 
-var s1 = new UConnect(107);
-var s2 = new UConnect(107);
+ScheduleMethod s1 = new BlockDesign(21);
+ScheduleMethod s2 = new BlockDesign(7);
 
-List<int> schedule1 = s1.Schedule; 
-List<int> schedule2 = s2.Schedule;
+HeterogenousRPC.NDTMetrics(s1, s2);
 
-for (int i = 0; i < schedule1.Count; i++)
-{
-    for (int j = i; j < schedule2.Count; j++)
-    {
-        int encounterIndex = hasEncounter(schedule1, schedule2, i, j);
-
-        if (encounterIndex == -1)
-        {
-            Console.WriteLine($"No encounter oportunity at schedule1 offset:{i} and schedule2 offset:{j}.");
-            Console.WriteLine(String.Empty);
-        }
-    }
-}
-
-
-static int hasEncounter(List<int> schedule1, List<int> schedule2, int offset1 = 0, int offset2 = 0)
-{
-    for(int i = 0; i < lcm(schedule1.Count, schedule2.Count); i++)
-    {
-        int index1 = offsetIndex(schedule1.Count, i + offset1);
-        int index2 = offsetIndex(schedule2.Count, i + offset2);
-
-        if(schedule1[index1] == 1 && schedule2[index2] == 1)
-        {
-            return i;
-        }
-    }
-
-    return -1;
-}
-
-static int gcf(int a, int b)
-{
-    while (b != 0)
-    {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
-static int lcm(int a, int b)
-{
-    return (a / gcf(a, b)) * b;
-}
-
-static int offsetIndex(int size, int offset = 0)
-{
-    return offset < size? offset : offset % size;
-}
 
 static void print(List<int> bd, int offset = 0)
 {
