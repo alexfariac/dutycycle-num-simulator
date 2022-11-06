d69479dab47e3f62e47ab186909f5f93b1cd4d55
Alexandre Cardoso <alexandre.faria95@gmail.com>
2022-11-06 12:41:53 -0300

    feat: adding prime math funcions

--- Assimetric-RPC/Assimetric-RPC/Utils/MathUtils.cs
+++ Assimetric-RPC/Assimetric-RPC/Utils/MathUtils.cs
@@ -18,5 +18,40 @@
             return a / gcf(a, b) * b;
         }
 
+        //function to check if a given number is prime
+        public static bool isPrime(int n)
+        {
+            //since 0 and 1 is not prime return false.
+            if (n == 1 || n == 0) return false;
+
+            //Run a loop from 2 to square root of n.
+            for (int i = 2; i * i <= n; i++)
+            {
+                // if the number is divisible by i, then n is not a prime number.
+                if (n % i == 0) return false;
+            }
+            //otherwise, n is prime number.
+            return true;
+        }
+
+        public static List<int> getPrimes(int n)
+        {
+            List<int> primes = new ();
+            
+            int prime = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                while (!isPrime(prime))
+                {
+                    prime++;
+                }
+                primes.Add(prime);
+                prime++;
+            }
+
+            return primes;
+        }
+
     }
 }
