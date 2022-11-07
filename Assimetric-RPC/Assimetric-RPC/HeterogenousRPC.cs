4ae01c93dcee066e0fa2a0af501e9791b50bd7a6
Alexandre Cardoso <alexandre.faria95@gmail.com>
2022-11-07 18:47:08 -0300

    feat: considering initial slot pair over offset

--- Assimetric-RPC/Assimetric-RPC/HeterogenousRPC.cs
+++ Assimetric-RPC/Assimetric-RPC/HeterogenousRPC.cs
@@ -10,42 +10,71 @@ namespace Assimetric_RPC
             int discoveryTimeSum = 0;
             int maxDiscoveryTime = 0;
             int minDiscoveryTime = int.MaxValue;
+            int iterations = 0;
 
             int lcm = MathUtils.Lcm(schedule1.ScheduleSize, schedule2.ScheduleSize);
 
-            for (int relativeOffset = 0; relativeOffset < lcm; relativeOffset++)
+            for(int startIndex1 = 0; startIndex1 < lcm; startIndex1++)
             {
-                int discoveryTime = DiscoveryTime(schedule1, schedule2, relativeOffset, lcm);
-                if(discoveryTime == -1)
+                for(int startIndex2 = startIndex1; startIndex2 < lcm; startIndex2++)
                 {
-                    Console.WriteLine($"S1: {schedule1.Name}, S2: {schedule2.Name} DC1: {schedule1.DutyCyclePerc}, DC2: {schedule2.DutyCyclePerc}, MIN-NDT: inf, AVG-NDT: inf, MAX-NDT: inf");
-                    return;
-                }
+                    int discoveryTime = DiscoveryTime(schedule1, schedule2, startIndex1, startIndex2, lcm);
+                    if (discoveryTime == -1)
+                    {
+                        Console.WriteLine($"S1: {schedule1.Name}, S2: {schedule2.Name} DC1: {schedule1.DutyCyclePerc}, DC2: {schedule2.DutyCyclePerc}, MIN-NDT: inf, AVG-NDT: inf, MAX-NDT: inf");
+                        return;
+                    }
 
-                discoveryTimeSum += discoveryTime;
-                maxDiscoveryTime = Math.Max(maxDiscoveryTime, discoveryTime);
-                minDiscoveryTime = Math.Min(minDiscoveryTime, discoveryTime);
+                    discoveryTimeSum += discoveryTime;
+                    maxDiscoveryTime = Math.Max(maxDiscoveryTime, discoveryTime);
+                    minDiscoveryTime = Math.Min(minDiscoveryTime, discoveryTime);
+                    iterations++;
+                }
             }
 
-            var averageNdt = discoveryTimeSum / lcm;
+            var averageNdt = discoveryTimeSum / iterations;
             Console.WriteLine($"S1: {schedule1.Name}, S2: {schedule2.Name} DC1: {schedule1.DutyCyclePerc}, DC2: {schedule2.DutyCyclePerc}, MIN-NDT: {minDiscoveryTime}, AVG-NDT: {averageNdt}, MAX-NDT: {maxDiscoveryTime}");
         }
 
-        public static int DiscoveryTime(ScheduleMethod schedule1, ScheduleMethod schedule2, int relativeOffset = 0, int lcm = -1)
+        public static int DiscoveryTime(ScheduleMethod schedule1, ScheduleMethod schedule2, int startIndex1 = 0, int startIndex2 = 0, int lcm = -1)
         {
             int schedule_lcm = lcm == -1 ? MathUtils.Lcm(schedule1.ScheduleSize, schedule2.ScheduleSize) : lcm;
             for (int i = 0; i < schedule_lcm; i++)
             {
-                int index1 = i % schedule1.ScheduleSize;
-                int index2 = (i + relativeOffset) % schedule2.ScheduleSize;
+                int index1 = (i + startIndex1) % schedule1.ScheduleSize;
+                int index2 = (i + startIndex2) % schedule2.ScheduleSize;
 
-                if(schedule1.Schedule[index1] && schedule2.Schedule[index2])
+                if (schedule1.Schedule[index1] && schedule2.Schedule[index2])
                 {
                     return i;
                 }
             }
 
             return -1;
+        }
+
+        public static List<int> CommonActiveSlots(ScheduleMethod s1, ScheduleMethod s2, int s1InitialSlot = 0, int s2InitialSlot = 0, int lcm = -1)
+        {
+            int schedule_lcm = lcm == -1 ? MathUtils.Lcm(s1.ScheduleSize, s2.ScheduleSize) : lcm;
+
+            List<int> intersectionSlots = new List<int>();
+
+            for(int i = 0; i < schedule_lcm; i++)
+            {
+                int index1 = (i + s1InitialSlot) % s1.ScheduleSize;
+                int index2 = (i + s2InitialSlot) % s2.ScheduleSize;
+
+                if(s1.Schedule[index1] && s2.Schedule[index2])
+                {
+                    intersectionSlots.Add(1);
+                }
+                else
+                {
+                    intersectionSlots.Add(0);
+                }
+            }
+
+            return intersectionSlots;
         }
 
     }
--- Assimetric-RPC/Assimetric-RPC/Program.cs
+++ Assimetric-RPC/Assimetric-RPC/Program.cs
@@ -2,14 +2,10 @@
 using Assimetric_RPC.Methods;
 using Assimetric_RPC.Utils;
 using System.Diagnostics;
-using System.Text;
-
-// ScheduleMethod s1 = new UConnect(21);
-// ScheduleMethod s2 = new Disco(137,199);
 
 Stopwatch sw = Stopwatch.StartNew();
 
-List<int> primesList = MathUtils.getPrimes(50);
+List<int> primesList = MathUtils.getPrimes(15);
 Console.WriteLine($"Primes generated in {sw.ElapsedMilliseconds} ms.");
 
 for (int i = 0; i < primesList.Count; i++)
@@ -29,15 +25,3 @@ for (int i = 0; i < primesList.Count; i++)
 
 
 Console.WriteLine($"Simulation executed in {sw.Elapsed.TotalSeconds} seconds.");
-
-
-static void print(List<int> bd, int offset = 0)
-{
-    StringBuilder sb = new ();
-    for (int i = 0; i < bd.Count; i++)
-    {
-        int index = (offset + i < bd.Count) ? offset + i : (offset + i) % bd.Count;
-        sb.Append(bd[index]);
-    }
-    Console.WriteLine(sb.ToString());
-}
