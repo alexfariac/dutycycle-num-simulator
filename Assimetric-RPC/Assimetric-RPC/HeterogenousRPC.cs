d9517788fd1515cfeef3377da87470d2c2ae1776
Alexandre Cardoso <alexandre.faria95@gmail.com>
2022-11-10 16:45:38 -0300

    feat: adding metrics and calculating average on by first schedule interation to avoid long overflow

--- Assimetric-RPC/Assimetric-RPC/HeterogenousRPC.cs
+++ Assimetric-RPC/Assimetric-RPC/HeterogenousRPC.cs
@@ -9,16 +9,18 @@ namespace Assimetric_RPC
 
         public static void NDTMetrics(ScheduleMethod schedule1, ScheduleMethod schedule2)
         {
-            ulong discoveryTimeSum = 0;
+            ulong discoveryTimeAverage = 0;
             int maxDiscoveryTime = 0;
-            int iterations = 0;
+            uint iterations = 0;
 
             int lcm = MathUtils.Lcm(schedule1.ScheduleSize, schedule2.ScheduleSize);
             int gcf = MathUtils.gcf(schedule1.ScheduleSize, schedule2.ScheduleSize);
 
             for (int startIndex1 = 0; startIndex1 < lcm; startIndex1++)
             {
-                for(int startIndex2 = startIndex1; startIndex2 < lcm; startIndex2++)
+                ulong discoveryTimeSum = 0;
+
+                for (int startIndex2 = startIndex1; startIndex2 < lcm; startIndex2++)
                 {
                     int discoveryTime = DiscoveryTime(schedule1, schedule2, startIndex1, startIndex2, lcm);
                     if (discoveryTime == -1)
@@ -27,14 +29,18 @@ namespace Assimetric_RPC
                         return;
                     }
 
-                    discoveryTimeSum += (ulong)discoveryTime;
+                    discoveryTimeSum += (uint)discoveryTime;
                     maxDiscoveryTime = Math.Max(maxDiscoveryTime, discoveryTime);
                     iterations++;
                 }
+
+                discoveryTimeAverage += discoveryTimeSum;
+
             }
 
-            double averageNdt = Math.Round((double)discoveryTimeSum / iterations, ROUND_PRECISION);
-            Console.WriteLine($"S1: {schedule1.Name}; S2: {schedule2.Name}; gcf: {gcf}; DC1: {Math.Round(schedule1.DutyCyclePerc, ROUND_PRECISION)}; DC2: {Math.Round(schedule2.DutyCyclePerc, ROUND_PRECISION)}; AVG-NDT: {averageNdt}; MAX-NDT: {maxDiscoveryTime}");
+            // Console.WriteLine($"DISCOVERY SUM: {discoveryTimeSum}, ITERATIONS: {iterations}");
+            double averageNdt = discoveryTimeAverage / iterations;
+            Console.WriteLine($"S1: {schedule1.Name}; S2: {schedule2.Name}; lcm: {lcm}; gcf: {gcf}; DC1: {Math.Round(schedule1.DutyCyclePerc, ROUND_PRECISION)}; DC2: {Math.Round(schedule2.DutyCyclePerc, ROUND_PRECISION)}; AVG-NDT: {averageNdt}; MAX-NDT: {maxDiscoveryTime}; ITERATIONS: {iterations}");
         }
 
         public static int DiscoveryTime(ScheduleMethod schedule1, ScheduleMethod schedule2, int startIndex1 = 0, int startIndex2 = 0, int lcm = -1)
